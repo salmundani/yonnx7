66403557b65591f65d2ac36657bdb05f1cbaa1d2
Daniel Salmun <salmundani@gmail.com>
2023-12-20 09:12:40 -0800

    Implemented Pow operation (without broadcast) + WIP Split operation

--- main.cc
+++ main.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d0bca730413e3d60563c09c741c44cdab37895c3755d4e39b276dc0424892a3
-size 6551
+oid sha256:79a318eea54b6ee9bb7afd8a5af6314ac059fbc59b9271e4188e7033b539c9fe
+size 8025
