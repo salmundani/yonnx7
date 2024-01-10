27dd4db691067570292ad42d3ec865075ddcfd85
Daniel Salmun <salmundani@gmail.com>
2024-01-10 10:29:18 -0300

    Refactor + transpose implemented

--- main.cc
+++ main.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06a38ff85e6a5e12691422755997717e3058ef1debe6ddc762e27c3caaa56c5a
-size 6591
+oid sha256:0ea059a8964f44ae86d69a6b785cd5dfc71d4eef1ac0f96de2ec8471d486748d
+size 6499
