bf20a21f7dad0f1fcc62a547f1ea736f0c6333a6
Daniel Salmun <salmundani@gmail.com>
2024-01-11 17:33:16 -0300

    MaxPool implemented

--- main.cc
+++ main.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69b57952ed81e3b2cd226f704b63df394268a07ceaa798275a1f2527c222d742
-size 9289
+oid sha256:d4d2f3b6b555e5696ab6a3dea2b952a940b1a7425a8f547db515cb7e9c0e97d1
+size 11257
