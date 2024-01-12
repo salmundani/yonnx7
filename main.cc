afbd446c6ece216e1af131f414f1102629d60bff
Daniel Salmun <salmundani@gmail.com>
2024-01-12 11:27:48 -0300

    Sigmoid implemented

--- main.cc
+++ main.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ea0b6e61dcc4a3d97124f7ebab77d3669f9c576f129a1519e155f34e73df36a
-size 11415
+oid sha256:466079150e2a21907c70bda23963ca13b4db1c1c50edf5b2731b80f788cf545d
+size 11536
