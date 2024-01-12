8b83d2f94c8980097a8650b769b3089cf3a28b15
Daniel Salmun <salmundani@gmail.com>
2024-01-12 11:27:09 -0300

    Pad tensor in MaxPool

--- main.cc
+++ main.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4d2f3b6b555e5696ab6a3dea2b952a940b1a7425a8f547db515cb7e9c0e97d1
-size 11257
+oid sha256:8ea0b6e61dcc4a3d97124f7ebab77d3669f9c576f129a1519e155f34e73df36a
+size 11415
