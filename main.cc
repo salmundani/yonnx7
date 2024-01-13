6ea47318c21f7b0f8bfb8c433dfe0feea7bc770d
Daniel Salmun <salmundani@gmail.com>
2024-01-13 16:59:08 -0300

    Read test image with libjpeg-turbo

--- main.cc
+++ main.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8968f8ef0396078f9692779c355471b4c02105a6503404a2ffef81e83b9f7208
-size 17990
+oid sha256:78fd94fbe0ed479c0e47b3b92b6a989937caab6eff4e1fc49cdd5ef5977dbc41
+size 20479
