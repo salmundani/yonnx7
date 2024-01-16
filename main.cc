39bc0eac322dffd08d24a3d4ad02506aa05e7d20
Daniel Salmun <salmundani@gmail.com>
2024-01-16 13:15:51 -0300

    Change input format from height x width x channels to channels x height x width

--- main.cc
+++ main.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb060ecc349af10423be4ab4162a3b1e25ec47cf6a1065d78fec466631de95e0
-size 22167
+oid sha256:c233ac6c3ff8d5e5c11a271c74d7fd605ebc00e46ea511ef29f4fe71a4d6f4c8
+size 22258
