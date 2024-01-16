88438c665344df595574521f37a5467e320a40e1
Daniel Salmun <salmundani@gmail.com>
2024-01-16 16:23:47 -0300

    Scripts: filter_csv and onnx_run

---
+++ scripts/filter_csv.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9ff3d6195dea1741535ea725f5221f07648b6ae491c3ae472b5064869a5ec381
+size 217
---
+++ scripts/onnx_run.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:92da0d3af34258cdfaf87bde047156fc78297c67be135255aea5b45a22fe8644
+size 705
