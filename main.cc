a9bb4afcfbbd758c4ba90f6d6986b0b1f593512f
Daniel Salmun <salmundani@gmail.com>
2024-01-16 12:21:58 -0300

    Normalize input values from 0-255 to 0-1

--- main.cc
+++ main.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50e341ffb9ec3bd1524425b95e73ebe2f99ffec558955ed18bc967116af3be08
-size 22068
+oid sha256:fb060ecc349af10423be4ab4162a3b1e25ec47cf6a1065d78fec466631de95e0
+size 22167
