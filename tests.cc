2e2f01bc9ef03420c3f8aabbc8699595e2c93199
Daniel Salmun <salmundani@gmail.com>
2024-01-16 23:45:39 -0300

    Added Conv tests

--- tests.cc
+++ tests.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:308774fa40112e109ff8abf41eaf6934228d45787af424fc34ddb6ded9f25579
-size 4648
+oid sha256:96ccf9e6c1f936078d2d50fac43064a8ca1477f17402192fdc3f5ac058668c4a
+size 11069
