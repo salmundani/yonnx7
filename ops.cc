b4bf3c1b553d2f01074adf5e46a2583d7582b11f
Daniel Salmun <salmundani@gmail.com>
2024-01-16 21:31:34 -0300

    Fix MaxPool

--- ops.cc
+++ ops.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10f6b1f4c0bb6064de233c9a27293fcd44d65b198334a45a6e03804a7bd668b7
-size 7793
+oid sha256:73c5e20986cdba6a2eaa9113f622b3d0283f155c219452eda5cc25763d8778ac
+size 7877
