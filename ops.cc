5436d8aec4e15bd0bfdee4376a4c403fb1db26a7
Daniel Salmun <salmundani@gmail.com>
2024-01-18 12:24:48 -0300

    Reduced the amount of xtensor copies in Conv

--- ops.cc
+++ ops.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab91afa60d5a98dba5f0ebb6c36e783187eb3c834f491a22b7340bc3671979c3
-size 7777
+oid sha256:3d71de50cc984a08cfbe5e9a3a94deced496452d59a6c8141092e9fdf7132272
+size 7879
