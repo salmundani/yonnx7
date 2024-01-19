a9b3c7a9606294b2c93353cffca7d7a21c037701
Daniel Salmun <salmundani@gmail.com>
2024-01-19 01:19:06 -0300

    Small refactors

--- ops.cc
+++ ops.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d71de50cc984a08cfbe5e9a3a94deced496452d59a6c8141092e9fdf7132272
-size 7879
+oid sha256:53b9a0f305c67ac76fda01bbec9d789a2b094e7fbd39a2c5e80eced2de0a0963
+size 7901
