dc1714cf5b22db8bee4b7668b8fbe4534ade7f98
Daniel Salmun <salmundani@gmail.com>
2024-02-02 13:17:19 -0300

    Replaced xtensor sigmoid with handmade solution for performance reasons

--- main.cc
+++ main.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81e2b617c2bef950a45459b50711965c13f911a141b6f42e9b2268f7f095e04f
-size 12477
+oid sha256:2cbaf7f72d6fe8cab2d4911e97f37c1673c6c667488a7093afb5e526ed0ac817
+size 12773
