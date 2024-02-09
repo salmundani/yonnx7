55e26056afef59db58ea17fcbe9d0dbbc14b8b79
Daniel Salmun <salmundani@gmail.com>
2024-02-09 16:02:37 -0300

    Improved CMakeLists.txt portability, removed BLIS code from main.cc

--- main.cc
+++ main.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2cbaf7f72d6fe8cab2d4911e97f37c1673c6c667488a7093afb5e526ed0ac817
-size 12773
+oid sha256:42132edfb0e99eb809cd630556a1e5c7b9df1926a6230e5bf2e337aae6a39cda
+size 12671
