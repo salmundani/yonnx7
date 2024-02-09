79a96fb0bbf1cb4e53bdeb8bede4b9da8652bb64
Daniel Salmun <salmundani@gmail.com>
2024-02-09 16:02:59 -0300

    Added toolchains files for cross-compiling to RISC-V

---
+++ toolchains/riscv64-clang.toolchain.cmake
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:be1ab9de57da2ebe0ebec30c44cee649f670f93024ec5bce4791c8a3c9cbab89
+size 1169
---
+++ toolchains/riscv64-gcc.toolchain.cmake
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:80ac1b7d5ccd7be50263cc76eefdb366c074eadc81679b29a0350a6064c8b08c
+size 1125
