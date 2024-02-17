a78c960ffaa6d37a85eca84b801f9fbdad013069
Daniel Salmun <salmundani@gmail.com>
2024-02-17 15:08:13 -0300

    Run multiple images in main.cpp

--- main.cc
+++ main.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c247db5433565ead46b272deeb3a643e576922130157c9c3ea5ec27bbf8928a
-size 15049
+oid sha256:d8c282ffab0cea7c638fdba704b9afdf1245eb0e7787077df2017890a9c9569a
+size 15991
