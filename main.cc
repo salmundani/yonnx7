e353db6de2e157bcf2c37143661385c29c033026
Daniel Salmun <salmundani@gmail.com>
2024-02-18 22:38:05 -0300

    Run experiment script

--- main.cc
+++ main.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d35b4a0a7336ccb7814f5100599457883f10cb4c5ad091ee82129c407b4901fb
-size 20861
+oid sha256:2b1ee56101ac6383ad62513ff14edf65306def9ece4f580f273a4ecd60b4a054
+size 20853
---
+++ scripts/run_experiments.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e907fc9b21700e2a393c3e15f19331e0bacaa0e0b308a05d76d73dea4a581a2e
+size 2223
